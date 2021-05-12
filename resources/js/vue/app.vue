27e79d204ecc184a992a32f98007900773bcd6fb
Bikash Shrestha <becas4th@icloud.com>
2021-05-12 13:41:44 +0900

    test

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -2,7 +2,7 @@
     <div class="todolistContainer">
         <div class="heading">
             <h2 id="title">
-            Tester Todo List
+            Test Todo List
             </h2>
             <add-item-form 
                 v-on:reloadlist="getList()"
